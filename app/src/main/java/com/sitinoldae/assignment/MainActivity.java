1debf025e813508bdcb55f7f213c0247e28466d9
Nitin alias Mehul <sitinoldae@gmail.com>
2019-09-14 14:14:42 +0530

    UI changes and Updates
    Open button, video player added

--- app/src/main/java/com/sitinoldae/assignment/MainActivity.java
+++ app/src/main/java/com/sitinoldae/assignment/MainActivity.java
@@ -5,9 +5,12 @@ import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
@@ -19,15 +22,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Objects;
@@ -36,29 +41,32 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private AlertDialog progressDialog;
-    private Button button,button2;
-    private ImageView imageView;
+    private Button open,download,paste;
+    private ImageView imagePreview;
+    private VideoView videoPreview;
     private Toast toast;
     private boolean isvideo;
     private boolean isImage;
     private String fileName;
+    //dec fetch
     private Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText= findViewById(R.id.editText);
-        button= findViewById(R.id.button2);
-        button2= findViewById(R.id.button);
-        imageView= findViewById(R.id.imageView);
+        editText= findViewById(R.id.edittext);
+        open= findViewById(R.id.openBtn);
+        download= findViewById(R.id.downloadBtn);
+        paste= findViewById(R.id.pasteBtn);
+        imagePreview= findViewById(R.id.imagePreview);
+        videoPreview= findViewById(R.id.videoPreview);
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         progressDialog = new AlertDialog.Builder(MainActivity.this).create();
         progressDialog.setTitle("Please wait");
         progressDialog.setMessage("The image is being downloaded !");
-
         toast=Toast.makeText(getApplicationContext(),"null",Toast.LENGTH_SHORT);
         //adding listeners
-        button2.setOnClickListener(new View.OnClickListener() {
+        paste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
@@ -72,14 +80,30 @@ public class MainActivity extends AppCompatActivity {
                 //paste data from clipboard
             }
         });
-        button.setOnLongClickListener(new View.OnLongClickListener() {
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //opening the url to a viewer
+                String CurrentUrl=editText.getText().toString().trim();
+                if(!CurrentUrl.isEmpty()){
+                    //do something
+                    if(!CurrentUrl.contains("htt")){
+                        CurrentUrl="http://"+CurrentUrl;
+                    }
+                    checkfiletype(CurrentUrl,true);
+                }else{
+                    toaster("insert url first");
+                }
+            }
+        });
+        paste.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
                 editText.setText("https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4");
                 return true;
             }
         });
-        button.setOnClickListener(new View.OnClickListener() {
+        download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                String url = editText.getText().toString().trim();
@@ -88,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                   url="http://"+url;
                 }
                 if(isWriteStoragePermissionGranted()){
-                    checkfiletype(url);
+                    checkfiletype(url,false);
                 }else{
                     toaster("permission denied");
                 }
@@ -96,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -131,27 +154,28 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onBitmapLoaded( Bitmap bitmap, Picasso.LoadedFrom from) {
                     saveImageToExternalStorage(bitmap);
-                    imageView.setImageBitmap(bitmap);
+                    imagePreview.setImageBitmap(bitmap);
                     progressDialog.dismiss();
                 }
 
                 @Override
                 public void onBitmapFailed(Exception e, Drawable errorDrawable) {
                     toaster("downloading failed with format :"+extension+currentURL);
-                    imageView.setImageDrawable(errorDrawable);
-                    Picasso.get().load(currentURL).into(imageView);
+                    imagePreview.setImageDrawable(errorDrawable);
+                    Picasso.get().load(currentURL).into(imagePreview);
                     progressDialog.dismiss();
                 }
 
                 @Override
                 public void onPrepareLoad(Drawable placeHolderDrawable) {
-                    imageView.setImageDrawable(placeHolderDrawable);
+                    imagePreview.setImageDrawable(placeHolderDrawable);
                     toaster("loading...");
                     progressDialog.show();
                 }
             });
         }
         if(isvideo){
+            toaster("video determined");
             //download video
             Uri uri = Uri.parse(url);
             DownloadManager.Request r = new DownloadManager.Request(uri);
@@ -161,7 +185,27 @@ public class MainActivity extends AppCompatActivity {
             DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
             try {
                 Objects.requireNonNull(dm).enqueue(r);
-                toaster("downloading "+fileName);
+                toaster("downloading...");
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("downloading "+fileName);
+                alertDialog.setMessage("do you want to play it as well ?");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                MediaController mediaController = new MediaController(getApplicationContext());
+                                videoPreview.setMediaController(mediaController);
+                                videoPreview.setVideoURI(Uri.parse(url));
+                                videoPreview.start();
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                alertDialog.show();
                 vibrator.vibrate(400);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -190,7 +234,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void checkfiletype(String url) {
+    private void checkfiletype(String url, Boolean open) {
         toaster("checking file type");
         if(url.contains(".")) {
             String extension = url.substring(url.lastIndexOf(".")).toLowerCase();
@@ -205,6 +249,9 @@ public class MainActivity extends AppCompatActivity {
                 case ".gif":
                     isImage=true;
                     toaster("image file found");
+                    if(open==true){
+                        openFile(url);
+                    }else
                     downloadFile(url,extension);
                     break;
                 case ".mp4":
@@ -212,7 +259,10 @@ public class MainActivity extends AppCompatActivity {
                 case ".3gp":
                     isvideo=true;
                     toaster("video file found");
-                    downloadFile(url, extension);
+                    if(open==true){
+                        openFile(url);
+                    }else
+                        downloadFile(url, extension);
                     break;
                 default:
                     toaster("invalid link: "+url+extension);
@@ -221,6 +271,40 @@ public class MainActivity extends AppCompatActivity {
         }else {
             toaster("invalid url : "+url);
         }
+    }
+
+    private void openFile(String url) {
+        //open file
+        if(isImage){
+            Picasso.get().load(Uri.parse(url)).networkPolicy(NetworkPolicy.NO_CACHE).into(imagePreview);
+        }
+        if(isvideo){
+            toaster("video file found");
+            videoPreview.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                @Override
+                public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                    mediaPlayer.release();
+                    toaster("error");
+                    return true;
+                }
+            });
+            videoPreview.setVideoPath(url);
+            videoPreview.start();
+            toaster("playing video");
+        }
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse(url);
+                intent.setData(uri);
+                startActivity(intent);
+
+            }
+        };
+        isvideo=false;
+        isImage=false;
     }
 
     private void toaster(String message){
@@ -258,11 +342,9 @@ public class MainActivity extends AppCompatActivity {
         // Tell the media scanner about the new file so that it is
         // immediately available to the user.
         MediaScannerConnection.scanFile(this, new String[] { file.toString() }, null,
-                new MediaScannerConnection.OnScanCompletedListener() {
-                    public void onScanCompleted(String path, Uri uri) {
-                        Log.i("ExternalStorage", "Scanned " + path + ":");
-                        Log.i("ExternalStorage", "-> uri=" + uri);
-                    }
+                (path, uri) -> {
+                    Log.i("ExternalStorage", "Scanned " + path + ":");
+                    Log.i("ExternalStorage", "-> uri=" + uri);
                 });
 
     }
